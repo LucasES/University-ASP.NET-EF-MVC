0355a409ddaa39f554d847039c2c682798de200d
LucasES <lucas.compufc@gmail.com>
2015-10-21 16:03:02 -0200

    Criação departamentos

--- UFCQuixada/DAL/SchoolContext.cs
+++ UFCQuixada/DAL/SchoolContext.cs
@@ -26,6 +26,7 @@ namespace UFCQuixada.DAL
                 .Map(t => t.MapLeftKey("CourseID")
                     .MapRightKey("InstructorID")
                     .ToTable("CourseInstructor"));
+            modelBuilder.Entity<Department>().MapToStoredProcedures();
         }
     }
 }
